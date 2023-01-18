e52b7f5b28d3c6b9cd494dac8f00c33973299129
mhmd <mzalshanti01@gmail.com>
2023-01-18 22:35:41 +0200

    Employee Validation

--- human_resource/human_resource/doctype/employeee/employeee.js
+++ human_resource/human_resource/doctype/employeee/employeee.js
@@ -2,7 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employeee', {
-	// refresh: function(frm) {
-
-	// }
+	validate(frm) {
+    if (frm.doc.date_of_birth >= frappe.datetime.get_today()) {
+     frappe.throw('You can not select date of birth after today');
+    }
+    var emp_dob = moment (frappe.datetime.now_date());
+    var current_date = moment(frm.doc.date_of_birth);
+    frm .doc.age =  emp_dob.year()- current_date.year();
+}
 });
