9e434f5bd330007dff42b9c7bb5950f2aea1f84f
mhmd <mzalshanti01@gmail.com>
2023-01-19 19:56:48 +0200

    Human resource with validate

--- human_resource/human_resource/doctype/employeee/employeee.py
+++ human_resource/human_resource/doctype/employeee/employeee.py
@@ -5,4 +5,25 @@
 from frappe.model.document import Document
 
 class Employeee(Document):
-	pass
+	def validate_mobile(self):
+		if self.mobile:
+			if len(self.mobile) == 10 and self.mobile.startswith('059'):
+				pass
+			else:
+				frappe.throw("Invalid Mobile Number")
+
+	def full_name(self):
+		self.full_name = self.first_name+" "+self.middle_name+" "+self.last_name
+
+	def validate_education(self):
+		if len(self.emplopyee_education) < 2:
+			frappe.throw("required atleast 2 education levels")
+
+
+	def status(self):
+		if self.status == "Active" and self.age>=60:
+			frappe.throw("You cann't added")
+
+
+
+
