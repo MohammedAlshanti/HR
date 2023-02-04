713709079ceb4831c164fbe96b0bc55ad77bbd5e
mhmd <mzalshanti01@gmail.com>
2023-02-04 15:04:59 +0200

    Attendance System

--- human_resource/human_resource/doctype/attendance/attendance.py
+++ human_resource/human_resource/doctype/attendance/attendance.py
@@ -28,48 +28,8 @@ class Attendance(Document):
 			settings.working_hours_threshold_for_absent
 		)
 
-		# Add Grace period
-		check_in = self.check_in
-		check_out = self.check_out
-		if (cint(settings.late_entry_grace_period) == 0 and cint(settings.early_exit_grace_period) == 0):
-			check_in = self.check_in
-			check_out = self.check_out
-		elif (cint(settings.late_entry_grace_period) > 0 and cint(settings.early_exit_grace_period) == 0):
-			check_in = get_time_str(
-				to_timedelta(self.check_in) + timedelta(minutes=cint(settings.late_entry_grace_period)))
-		elif (cint(settings.late_entry_grace_period) == 0 and cint(settings.early_exit_grace_period) > 0):
-			# check_in = self.check_in
-			# get_time_str(
-			# 	to_timedelta(self.check_in) + timedelta(minutes=cint(settings.early_exit_grace_period)))
-			check_out = get_time_str(
-				to_timedelta(self.check_out) - timedelta(minutes=cint(settings.early_exit_grace_period)))
-		else:
-			check_in = get_time_str(
-				to_timedelta(self.check_in) - timedelta(minutes=cint(settings.late_entry_grace_period)))
-			check_out = get_time_str(
-				to_timedelta(self.check_out) + timedelta(minutes=cint(settings.early_exit_grace_period)))
-
-		working_hours = round(time_diff_in_hours(check_out, check_in),2)
-
-		if settings.start_time and settings.end_time:
-			req_hours = time_diff_in_hours(settings.end_time, settings.start_time)
-
-		if req_hours > 0 and req_hours > working_hours:
-			late_hours = req_hours - working_hours
-
-		if working_hours < working_hours_threshold_for_absent:
-			status = "Absent"
-		if is_validate:
-			self.work_hours = working_hours
-			self.late_hours = late_hours
-			self.status = status
-		else:
-			self.db_set("work_hours", working_hours)
-			self.db_set("late_hours", late_hours)
-			self.db_set("status", status)
-
-		late_entry_grace_period = 30
-		early_exit_grace_period = 15
+		# late_entry_grace_period = 30
+		# early_exit_grace_period = 15
 		status = "Present"
 		req_hours = 0
 		late_hours = 0
@@ -82,15 +42,18 @@ class Attendance(Document):
 		if (int(settings.late_entry_grace_period) == 0 and int(settings.early_exit_grace_period) == 0):
 			check_in = self.check_in
 			check_out = self.check_out
+			working_hours = time_diff_in_hours(check_out, check_in)
 		elif (int(settings.late_entry_grace_period) > 0 and int(settings.early_exit_grace_period) == 0):
-			check_in = get_time_str(to_timedelta(self.check_in) - timedelta(minutes=late_entry_grace_period))
+			check_in = get_time_str(to_timedelta(self.check_in) - timedelta(minutes=settings.late_entry_grace_period))
+			working_hours = time_diff_in_hours(check_out, check_in)
 		elif (int(settings.late_entry_grace_period) == 0 and int(settings.early_exit_grace_period) > 0):
-			check_out = get_time_str(to_timedelta(self.check_out) + timedelta(minutes=early_exit_grace_period))
+			check_out = get_time_str(to_timedelta(self.check_out) + timedelta(minutes=settings.early_exit_grace_period))
+			working_hours = time_diff_in_hours(check_out, check_in)
 		else:
-			check_in = get_time_str(to_timedelta(self.check_in) - timedelta(minutes=late_entry_grace_period))
-			check_out = get_time_str(to_timedelta(self.check_out) + timedelta(minutes=0))
+			check_in = get_time_str(to_timedelta(self.check_in) - timedelta(minutes=settings.late_entry_grace_period))
+			check_out = get_time_str(to_timedelta(self.check_out) + timedelta(minutes=0.0))
+			working_hours = time_diff_in_hours(check_out, check_in)
 
-		working_hours = time_diff_in_hours(check_out, check_in)
 
 		if settings.start_time and settings.end_time:
 			req_hours = time_diff_in_hours(settings.end_time, settings.start_time)
@@ -100,6 +63,7 @@ class Attendance(Document):
 
 		if working_hours < working_hours_threshold_for_absent:
 			status = "Absent"
+
 
 		if is_validate:
 			self.work_hours = working_hours
